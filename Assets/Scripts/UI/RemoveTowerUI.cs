3e1f7067ac6b94723292268b14808e045ad4c97c
Magdamini <mpabisz@student.agh.edu.pl>
2026-04-22 23:31:26 +0200

    Removing towers

--- Assets/Scripts/UI/RemoveTowerUI.cs
+++ Assets/Scripts/UI/RemoveTowerUI.cs
@@ -1,54 +1,20 @@
-﻿using UnityEngine;
-using UnityEngine.EventSystems;
-using TMPro;
+﻿using UnityEngine.EventSystems;
 
-public class RemoveTowerUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class RemoveTowerUI : InGameButtonUI
 {
-    //[Header("Tower Data")]
-    //[SerializeField] private GameObject towerPrefab;
-
-    [Header("UI")]
-    [SerializeField] private RectTransform image;
+    //[Header("UI")]
     //[SerializeField] private TextMeshProUGUI amountText;
+    private TowerMenu towerMenu;
 
-    [Header("Animation")]
-    [SerializeField] private float hoverScale = 1.2f;
-    [SerializeField] private float animationTime = 0.2f;
-
-    private Vector3 originalScale;
-
-    //private void Awake()
-    //{
-    //    originalScale = towerImage.localScale;
-    //}
-
-    public void OnPointerEnter(PointerEventData eventData)
+    private void Start()
     {
-        //LeanTween.cancel(image);
-        //LeanTween.scale(image, originalScale * hoverScale, animationTime)
-        //    .setEaseOutBack();
+        towerMenu = FindAnyObjectByType<TowerMenu>();
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    public override void OnPointerClick(PointerEventData eventData)
     {
-        //LeanTween.cancel(image);
-        //LeanTween.scale(image, originalScale, animationTime)
-        //    .setEaseInOutQuad();
-    }
+        base.OnPointerClick(eventData);
+        towerMenu.RemoveTower();
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        //LeanTween.cancel(image);
-
-        //// Click animation sequence
-        //LeanTween.scale(image, image.localScale * 1.2f, animationTime / 2 )
-        //    .setEaseInOutQuad()
-        //    .setOnComplete(() =>
-        //    {
-        //        LeanTween.scale(image, originalScale * hoverScale, animationTime / 2 )
-        //            .setEaseOutBack();
-        //    });
-
-        //BuildManager.Instance.StartPlacement(towerPrefab);
     }
 }
